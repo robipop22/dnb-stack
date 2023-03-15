b110bc23a2575b2ccb8e50be225b8d499b14c3ae
robipop22 <poprobertdaniel22@gmail.com>
2023-03-15 18:08:54 +0200

    change to new routing convention

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,6 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-	serverBuildTarget: 'vercel',
 	// When running locally in development mode, we use the built in remix
 	// server. This does not understand the vercel lambda module format,
 	// so we default back to the standard build output.
@@ -15,5 +14,6 @@ module.exports = {
 	future: {
 		v2_meta: true,
 		unstable_tailwind: true,
+		v2_routeConvention: true,
 	},
 };
