4731808c11a9b7548a4c267a1b1c3dbc3d92eda7
robipop22 <poprobertdaniel22@gmail.com>
2023-05-25 18:19:50 +0300

    fix final prettier and eslint things

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -17,7 +17,7 @@ const Index = () => (
 	<div className="flex h-screen w-screen flex-col items-center justify-start pt-20 text-center">
 		<h1 className="pb-10 text-7xl">Remix DnB Stack</h1>
 		<h2>This Stack contains a starter template for developing Remix Apps in vercel.</h2>
-		<h2 className="pt-10 pb-10">
+		<h2 className="pb-10 pt-10">
 			In the the following routes you will find an example of a loader. This stack works great with both your own api's
 			or a separate 3rd api.
 		</h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-import type { Config } from "tailwindcss";
-
 module.exports = {
 	content: ['./app/**/*.{ts,tsx,jsx,js}'],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
-}satisfies Config;
+};
