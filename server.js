798bd54a36c75c989d888d20c61bb03c3c5cd494
robipop22 <poprobertdaniel22@gmail.com>
2023-06-18 19:51:46 +0300

    update import for srever.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { createRequestHandler } from '@remix-run/vercel';
+import { createRequestHandler } from '@vercel/remix';
 import * as build from '@remix-run/dev/server-build';
 
 export default createRequestHandler({ build, mode: process.env.NODE_ENV });
