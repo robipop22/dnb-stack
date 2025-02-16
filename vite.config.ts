f2401e189d6fffbab3df45c1da237c45758a5070
robipop22 <poprobertdaniel22@gmail.com>
2025-02-17 00:49:30 +0200

    bump to tailwind rm postcss use vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@
 /// <reference types="vite/client" />
 
 import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { vercelPreset } from '@vercel/react-router/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	server: {
